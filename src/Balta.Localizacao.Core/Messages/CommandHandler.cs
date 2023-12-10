ca4cc9a6456d233afb2e24442fc0d382aa015b15
pedroMF1996 <pmfrp@hotmail.com>
2023-12-10 17:18:58 -0300

    Data, Message, Mediatr

---
+++ src/Balta.Localizacao.Core/Messages/CommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Balta.Localizacao.Core.Data;
+using Balta.Localizacao.Core.DomainObjects;
+using FluentValidation.Results;
+
+namespace Balta.Localizacao.Core.Messages
+{
+    public abstract class CommandHandler
+    {
+        public ValidationResult ValidationResult { get; private set; }
+
+        protected CommandHandler() 
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        public virtual void AdicionarErro(string msgErro) 
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(nameof(CommandHandler), msgErro));
+        }
+
+        public virtual bool PersistirDados<T>(IRepository<T> repository) where T : IAggregateRoot
+        {
+            if (!repository.unitOfWork.Commit())
+                AdicionarErro("Erro ao persistir dados");
+
+            return true;
+        }
+    }
+
+}
---
+++ src/Balta.Localizacao.Core/Messages/Message.cs
@@ -0,0 +1,15 @@
+﻿namespace Balta.Localizacao.Core.Messages
+{
+    public abstract class Message
+    {
+        public Guid AggregateId { get; private set; }
+        public string MessageType { get; private set; }
+
+        protected Message(string messageType)
+        {
+            AggregateId = Guid.NewGuid();
+            MessageType = messageType;
+        }
+    }
+
+}
