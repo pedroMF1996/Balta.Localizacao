bbe5c63aa4384493c0b6ca11ef19ff41f16ce650
pedroMF1996 <pmfrp@hotmail.com>
2023-12-11 11:52:22 -0300

    hotfix

--- src/Balta.Localizacao.Domain/DomainObjects/UserTokenModel.cs
+++ src/Balta.Localizacao.Domain/DomainObjects/UserTokenModel.cs
@@ -2,7 +2,7 @@
 {
     public class UserTokenModel
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string Email { get; set; }
         public List<ClaimModel> ClaimModels { get; set; }
 
