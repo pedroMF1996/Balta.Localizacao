3484198d6f55c36c4caeefe75fc0f32e9571d814
pedroMF1996 <pmfrp@hotmail.com>
2023-12-11 12:21:10 -0300

    LoginCommand

---
+++ src/Balta.Localizacao.ApplicationLayer/Commands/AutenticacaoCommands/LoginCommand.cs
@@ -0,0 +1,24 @@
+﻿using Balta.Localizacao.ApplicationLayer.Commands.AutenticacaoCommands.Validations;
+using Balta.Localizacao.Core.Messages;
+
+namespace Balta.Localizacao.ApplicationLayer.Commands.AutenticacaoCommands
+{
+    public class LoginCommand : Command
+    {
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        public LoginCommand(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new LoginCommandValidation().Validate(this);
+            return base.EhValido();
+        }
+    }
+}
