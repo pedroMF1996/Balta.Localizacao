f1cdd216d51cbb9e530e94d914de536863b7d919
pedroMF1996 <pmfrp@hotmail.com>
2023-12-11 13:23:22 -0300

    AppSetings

---
+++ src/Balta.Localizacao.Core/Identity/AppSetings.cs
@@ -0,0 +1,10 @@
+﻿namespace Balta.Localizacao.Core.Identity
+{
+    public class AppSetings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
