ede5589ad1c8c6b7006ad01e92f22ec394396cfb
Dhionys Soares <dhionys.soares@gmail.com>
2023-12-11 11:44:52 -0300

    Criação de Entities e MunicipioValidation
    Entities e MunicipioValitadion em desenvolvimento

---
+++ src/Balta.Localizacao.Domain/Entities/Estado.cs
@@ -0,0 +1,39 @@
+﻿using Balta.Localizacao.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balta.Localizacao.Domain.Entities
+{
+    internal class Estado : Entity, IAggregateRoot
+    {
+        public string CodigoUf { get; private set; }
+        public string SiglaUf { get; private set; }
+        public string NomeUf { get; private set; }
+        public List<Municipio> Municipios { get; private set; } = new();
+
+        protected Estado()
+        {
+
+        }
+        
+        public Estado(string codigoUf, string siglaUf, string nomeUf)
+        {
+            CodigoUf = codigoUf;
+            SiglaUf = siglaUf;
+            NomeUf = nomeUf;
+        }
+
+        public void AdicionarMunicipio(Municipio municipio)
+        {
+
+        }
+
+        public override bool EhValido()
+        {
+            return base.EhValido();
+        }
+    }
+}
---
+++ src/Balta.Localizacao.Domain/Entities/Municipio.cs
@@ -0,0 +1,38 @@
+﻿using Balta.Localizacao.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balta.Localizacao.Domain.Entities
+{
+    internal class Municipio : Entity
+    {
+        public string Codigo { get; private set; }
+        public string Nome { get; private set; }
+        public string CodigoUf { get; private set; }
+        public Estado Estado { get; private set; }
+
+        public Municipio()
+        {
+        }
+
+        public Municipio(string codigo, string nome, string codigoUf)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            CodigoUf = codigoUf;
+        }
+
+        public void AssociarEstado(Estado estado)
+        {
+
+        }
+
+        public override bool EhValido()
+        {
+            return base.EhValido();
+        }
+    }
+}
