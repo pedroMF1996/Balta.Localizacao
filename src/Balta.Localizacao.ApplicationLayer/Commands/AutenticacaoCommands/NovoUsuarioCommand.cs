64145cbd8675a4e04f7c1ff72a3653ac751b25ed
pedroMF1996 <pmfrp@hotmail.com>
2023-12-11 12:41:32 -0300

    NovoUsuarioCommand

--- src/Balta.Localizacao.ApplicationLayer/Commands/AutenticacaoCommands/NovoUsuarioCommand.cs
+++ src/Balta.Localizacao.ApplicationLayer/Commands/AutenticacaoCommands/NovoUsuarioCommand.cs
@@ -3,20 +3,19 @@ using Balta.Localizacao.Core.Messages;
 
 namespace Balta.Localizacao.ApplicationLayer.Commands.AutenticacaoCommands
 {
-    public class LoginCommand : Command
+    public class NovoUsuarioCommand : Command
     {
+        public string Nome { get; set; }
         public string Email { get; set; }
-
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
 
-        public LoginCommand()
-        {
-
-        }
+        public NovoUsuarioCommand()
+        {}
 
         public override bool EhValido()
         {
-            ValidationResult = new LoginCommandValidation().Validate(this);
+            ValidationResult = new NovoUsuarioCommandValidation().Validate(this);
             return base.EhValido();
         }
     }
---
+++ src/Balta.Localizacao.ApplicationLayer/Commands/AutenticacaoCommands/Validations/NovoUsuarioCommandValidation.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+
+namespace Balta.Localizacao.ApplicationLayer.Commands.AutenticacaoCommands.Validations
+{
+    public class NovoUsuarioCommandValidation : AbstractValidator<NovoUsuarioCommand>
+    {
+        public static int MAX_LENGHT_PASSWORD = 100;
+        public static int MIN_LENGHT_PASSWORD = 6;
+
+        public static string NomeRequiredErrorMessage = "O campo Nome é obrigatório";
+        public static string EmailRequiredErrorMessage = "O campo Email é obrigatório";
+        public static string EmailAddressErrorMessage = "O campo Email está em formato inválido";
+        public static string PasswordRequiredErrorMessage = "O campo Password é obrigatório";
+        public static string PasswordLenghtErrorMessage = $"O campo Password precisa ter entre {MIN_LENGHT_PASSWORD} e {MAX_LENGHT_PASSWORD} caracteres";
+        public static string ConfirmPasswordErrorMessage = "As senhas não conferem.";
+
+
+        public NovoUsuarioCommandValidation()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage(NomeRequiredErrorMessage);
+
+            RuleFor(c => c.Email)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage(EmailRequiredErrorMessage)
+               .EmailAddress()
+               .WithMessage(EmailAddressErrorMessage);
+
+            RuleFor(c => c.Password)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage(PasswordRequiredErrorMessage)
+                .MaximumLength(MAX_LENGHT_PASSWORD)
+                .MinimumLength(MIN_LENGHT_PASSWORD)
+                .WithMessage(PasswordLenghtErrorMessage);
+
+            RuleFor(c => c.ConfirmPassword)
+                .Equal(c => c.Password)
+                .WithMessage(ConfirmPasswordErrorMessage);
+        }
+    }
+}
