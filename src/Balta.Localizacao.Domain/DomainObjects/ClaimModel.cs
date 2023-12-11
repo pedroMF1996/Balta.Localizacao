c8f1606511bcf8bcf99de8a8344ebbb06fb2db44
pedroMF1996 <pmfrp@hotmail.com>
2023-12-11 11:50:40 -0300

    Criacao dos DomainObjects ClaimModel e UserTokenModel

---
+++ src/Balta.Localizacao.Domain/DomainObjects/ClaimModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Balta.Localizacao.Domain.DomainObjects
+{
+    public class ClaimModel
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+
+        public ClaimModel()
+        {
+            
+        }
+    }
+}
---
+++ src/Balta.Localizacao.Domain/DomainObjects/UserTokenModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Balta.Localizacao.Domain.DomainObjects
+{
+    public class UserTokenModel
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public List<ClaimModel> ClaimModels { get; set; }
+
+        public UserTokenModel()
+        {
+            
+        }
+    }
+}
