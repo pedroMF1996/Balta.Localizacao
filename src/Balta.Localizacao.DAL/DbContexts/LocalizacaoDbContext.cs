1cf621a9ed3530ef4854543993dc94ae03acc960
pedroMF1996 <pmfrp@hotmail.com>
2023-12-12 16:50:04 -0300

    LocalizacaoDbContext Criado

---
+++ src/Balta.Localizacao.DAL/DbContexts/LocalizacaoDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Balta.Localizacao.Core.Data;
+using Balta.Localizacao.Domain.Entities;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+
+namespace Balta.Localizacao.DAL.DbContexts
+{
+    public class LocalizacaoDbContext : DbContext, IUnitOfWork
+    {
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<Municipio> Municipios { get; set; }
+
+        public LocalizacaoDbContext(DbContextOptions<LocalizacaoDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public bool Commit()
+        {
+            var success = SaveChanges() > 0;
+            return success;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Ignore<ValidationResult>();
+            
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LocalizacaoDbContext).Assembly);
+        }
+    }
+}
