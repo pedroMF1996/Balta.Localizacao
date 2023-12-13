2881d111057e51ed25a96b1b05a4d4d585b32597
pedroMF1996 <pmfrp@hotmail.com>
2023-12-13 13:59:22 -0300

    Spacification Pattern

---
+++ src/Balta.Localizacao.Core/Spacification/SpecificationExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Balta.Localizacao.Core.Spacification
+{
+    public static class SpecificationExtensions
+    {
+
+        public static ISpacification<T> And<T> (this ISpacification<T> left, ISpacification<T> right)
+        {
+            return new AndSpacification<T>(left, right);
+        }
+
+        public static ISpacification<T> Or<T> (this ISpacification<T> left, ISpacification<T> right)
+        {
+            return new OrSpacification<T>(left, right);
+        }
+
+        public static ISpacification<T> Not<T>(this ISpacification<T> spacification)
+        {
+            return new NotSpacification<T>(spacification);
+        }
+
+    }
+}
